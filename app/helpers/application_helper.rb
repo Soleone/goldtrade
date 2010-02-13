cf2d08da96713f4193e1819312f9d26662bb6a8f
Dennis Theisen <dennis@jadedpixel.com>
2010-02-13 05:13:35 +0100

    Added Google Charts support for displaying price changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,19 @@ module ApplicationHelper
   def render_price(price)
     "%0.2f" % price 
   end
+  
+  def render_chart(stocks)
+    stocks = stocks.reverse
+    prices = stocks.collect(&:price)
+    price_range = prices.min..prices.max
+    chart = GoogleChart::LineChart.new
+    chart.title = "Price changes of #{stocks.first.name}"
+    chart.scale = price_range
+    chart.axes  = {:x => stocks.collect{|s| "#{s.created_at.day}."}, :y => price_range}
+    chart.type  = :sparkline
+    chart.size  = '400x100'
+    chart.data  = prices
+    chart.color = '00FF00'
+    image_tag(chart.to_url)
+  end
 end
